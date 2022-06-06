75f85b67267df1f5913e85c081d82f03ca4baf08
Foxpeet <diegobal007@gmail.com>
2022-06-06 16:57:34 +0200

    sumador

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+function sumar(){
+    var num1 = document.getElementById("numero1").value
+    var num2 = document.getElementById("numero2").value
+    document.getElementById("resultado").textContent = Number(num1)+Number(num2)
+    return
+}
